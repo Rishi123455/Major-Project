af7eaa3a6d512d7dfae6d5bd5db952e83e9f0d99
distroter <guptarishi123455@gmail.com>
2019-04-20 16:30:31 +0530

    first commit

---
+++ test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 19 19:53:42 2019
+
+@author: distroter
+"""
+import MySQLdb
+conn = MySQLdb.connect(user="root", passwd="rishi26071997", db="rishi")
+cur = conn.cursor()
+cur.execute("select Pwd from Credentials where userid = \"Abhay\" ;")
+row = cur.fetchone()
+print(row[0])
+conn.commit()
+cur.close()
+conn.close()
+
